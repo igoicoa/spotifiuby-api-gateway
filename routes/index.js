9cf9fb105c5ee0a17ddddbeec95652735d9c0790
igoicoa <igoicoa@hotmail.com>
2022-04-28 20:18:53 -0300

    Add multimedia and users endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,13 @@ const router = express.Router()
 const axios = require('axios');
 const services = require('./services.json');
 
-router.all('/:apiName/:path', (req, res) => {
+router.all('/:apiName/(*)', (req, res) => {
     const url = services.services[req.params.apiName].url
-    console.log("API: " + req.params.apiName + ". Path to invoke: " + url + req.params.path)
+    console.log("API: " + req.params.apiName + ". Path to invoke: " + url + req.params[0])
     axios({
         method: req.method,
-        url: url + req.params.path,
-        headers: req.headers,
-        data: req.body
-    }).then((response) => {
-        res.send(response.data)
-    }).catch(error => {
-        res.send(error)
-    })
-})
-
-router.all('/:apiName/:path/:id', (req, res) => {
-    const url = services.services[req.params.apiName].url
-    console.log("API: " + req.params.apiName + ". Path to invoke: " + url + req.params.path + '/' + req.params.id,)
-    axios({
-        method: req.method,
-        url: url + req.params.path + '/' + req.params.id,
-        headers: req.headers,
+        url: url + req.params[0],
+        //headers: req.headers,
         data: req.body
     }).then((response) => {
         res.send(response.data)
